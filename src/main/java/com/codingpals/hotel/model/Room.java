b7c66f1805560282f6e692a772ea4b90259614bd
Mahefa Ny Anjara <hei.mahefa@gmail.com>
2022-08-31 12:08:50 +0300

    feat: Room

--- src/main/java/com/codingpals/hotel/model/Room.java
+++ src/main/java/com/codingpals/hotel/model/Room.java
@@ -1,4 +1,43 @@
 package com.codingpals.hotel.model;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Room {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  @Enumerated(EnumType.STRING)
+  private Status status;
+  @ManyToOne
+  private RoomCategory category;
+  @Transient
+  private double price;
+  @Transient
+  private int bookingCount;
+
+  public double getPrice() {
+    return category.getPrice();
+  }
+
+  public enum Status {
+    AVAILABLE, TAKEN
+  }
 }
